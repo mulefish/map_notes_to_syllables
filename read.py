f3a81d69e2e323215b5a055d43521fc5690bed16
kermitt PDX <bluefishbrowndirt@gmail.com>
2020-12-29 12:52:13 -0600

    fourth pass

--- read.py
+++ read.py
@@ -3,6 +3,7 @@ import re
 
 def get_syllables(word):
     # given 'kittycat' return ['kit', 'ty', 'cat]
+    word = word.strip()
     word = word.lower()
     regex = '[^aeiouy]*[aeiouy]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?'
     x = re.findall(regex, word)
@@ -21,8 +22,21 @@ def get_lines_from_file(filename):
         count += 1 
     return lines
 
+def collect_syllables(words):
+    seen = {}
+    for word in words:
+        ary_of_syllables = get_syllables(word)
+        for syllable in ary_of_syllables:
+            if syllable in seen:
+                seen[syllable] += 1
+            else:
+                seen[syllable] = 1
+    return seen
 
-## 
+
+
+
 if __name__ == "__main__":
     filename = '10000.txt'
-    lines = get_lines_from_file(filename)
+    words = get_lines_from_file(filename)
+    collect_syllables(words)
--- test.py
+++ test.py
@@ -1,7 +1,48 @@
 import unittest
-from read import get_syllables
+from read import get_syllables, collect_syllables
 
 class TheTests(unittest.TestCase):
+
+    def test_get_collection_from_list_of_words(self):
+        given = [
+            'dinosaur',
+            'dynomite',
+            'mightymite',
+            'dog',
+            'dogs',
+            'doggerbank',
+            'dogface',
+            'kdlflkfakdakdk',
+            ''
+        ]
+        actual = collect_syllables(given)
+
+        expected = {
+            'ger':1, 
+            'no': 2, 
+            'di':  1,
+            'mi':  2,
+            'dog':  3,
+            'mig':  1, 
+            'fa':  1,
+            'ce':  1,   
+            'bank':  1,
+            'dy':  1,
+            'te':  2,
+            'dogs':  1, 
+            'hty':  1 
+        }
+
+        print("expected len {} ".format( len( expected )))
+        print("actual len {} ".format( len( actual )))
+        isOk = True 
+        for k in expected:
+            actual_count = actual[k]
+            expected_count = expected[k]
+            if actual_count is not expected_count:
+                isOk = False
+        self.assertEqual(True, isOk)
+
 
     def test_get_syllables(self):
         given = 'kittycat'
